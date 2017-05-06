bcab3b1591119933d8248c5db74eb00ec36275a9
danag <le_bonanni@yahoo.com.ar>
2017-05-06 19:46:42 -0300

    Multiple file upload

--- ws1/routes/FileUploadRoute.php
+++ ws1/routes/FileUploadRoute.php
@@ -2,30 +2,45 @@
 
 class FileUploadRoute{
     
-
     public $app;
-
+    
     public function __construct($app) {
        $this->app = $app;
     }
-
+    
     public function createRoutes(){
+        
         $this->app->group('/images/{id}',function(){
-            
             $this->map(['GET','DELETE','PUT'],'', function($request,$response,$args){
-                
             });
         });
         
         $this->app->post('/images[/]', function($request, $response, $args){
-           if (!empty($_FILES)){
-               $tempPath = $_FILES[ 'file' ][ 'tmp_name' ];
-               $uploadPath = dirname( __DIR__ . '/../../ ') . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $_FILES[ 'file' ][ 'name' ];
-               
-               move_uploaded_file($tempPath, $uploadPath);
-               
+           if (isset($_FILES)){
+                $imgs = array();
+            	$file_ary = array();
+            	$file_post = $_FILES['images'];
+                $cnt = count($_FILES);
+                $file_keys = array_keys($_FILES);
+                $i=0;
+               foreach($file_keys as $key) {
+                   $keys = array_keys($_FILES[$key]);
+                   foreach($keys as $filekey)
+                   {
+                       $file_ary[$i][$filekey] = $_FILES[$key][$filekey];
+                   }
+                   $i++;
+               }
+            for($i = 0 ; $i < $cnt ; $i++) {
+                     $name = uniqid('img-'.date('Ymd').'-');
+                       $tempPath = $file_ary[$i][ 'tmp_name' ];
+                        $dir = realpath(__DIR__ . '/..');
+                       $uploadPath = dirname( $dir ). DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $name . @'.' .pathinfo($file_ary[$i][ 'name' ], PATHINFO_EXTENSION);
+                       if(move_uploaded_file($tempPath, $uploadPath) === true){
+                           $imgs[] = array('url' => '/uploads/' . $name, 'name' => $file_ary[$i][ 'name' ]);
+                        }
+            }
                return $response;
-
            }
             else{
                 $newResponse = $response->withStatus(500);
@@ -33,5 +48,7 @@ class FileUploadRoute{
             }
         });
     }
+    
+
 }
 ?>
